20c96a3eebb1836ae3df8607e7cd5d2fdfb11745
lafeillou <lafeillou@126.com>
2020-04-24 13:04:46 +0800

    react-native-router-flux中的drawer用法实现

--- src/components/TargetObject.js
+++ src/components/TargetObject.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   View, Text, StyleSheet,
 } from 'react-native';
-import { Actions } from 'react-native-router-flux';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,20 +19,14 @@ export default class extends React.Component {
   }
 
   componentDidMount() {
-    // Actions.drawerOpen();
+
   }
 
-
   render() {
-    // const { children } = this.props;
     return (
-      <View style={{
-        position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'transparent',
-      }}
-      >
+      <View style={styles.container}>
         <Text style={{ fontSize: 40, color: '#000' }}>targetObject</Text>
       </View>
-    // children
     );
   }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {
-  Stack, Scene, Drawer, ActionConst,
+  Stack, Scene, Drawer,
 } from 'react-native-router-flux';
 
 
-import { Alert } from 'react-native';
 import LeafletMap from '../components/LeafletMap';
-import Login from '../components/Login';
+
 
 import TargetObject from '../components/TargetObject';
-import Test from '../components/Test';
 
-import { calc } from '../lib/utils';
 
 const Index = (
   <Stack key="root" hideNavBar>
@@ -27,18 +24,9 @@ const Index = (
       }}
       contentComponent={TargetObject}
     >
-      {/* <Scene hideNavBar key="login" component={Login} /> */}
       <Scene hideNavBar key="home" component={LeafletMap} />
-      {/* <Scene hideNavBar key="test" component={Test} /> */}
-
     </Drawer>
   </Stack>
-  // <Stack key="root" hideNavBar>
-  //   <Scene key="login" component={Login} />
-  //   <Scene key="home" component={LeafletMap} />
-  // </Stack>
-
-
 );
 
 export default Index;
